2d72c714532aa9302589f5919019f5220e1cde46
Mayte <mayte710@hotmail.com>
2024-04-17 11:02:06 +0100

    project structure

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import '../styles/App.scss';
+// Fichero src/components/App.jsx
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello girl</h1>
+    </div>
+  );
+};
+
+export default App;
